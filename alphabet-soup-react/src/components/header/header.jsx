a21c1592446f252c583e6b198c667cccaa59a1af
Paulitchos <74797330+Paulitchos@users.noreply.github.com>
2022-05-21 00:49:14 +0100

    Começar a meter o site como queremos
    usei maior parte código das aulas

---
+++ alphabet-soup-react/src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+    return (
+      <header>
+        <h1 className="title">Sopa de Letras</h1>
+        <h2 className="subtitle">Linguagens Script - 2022</h2>
+      </header>
+    );
+}
+
+export default Header;
